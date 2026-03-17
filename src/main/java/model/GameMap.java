aae15df6fc658fea1a6db86e3ab67894b96e77fe
Qian Gu <guqian202309@gmail.com>
2026-03-17 23:15:51 +0100

    Implement GameMap and GridPos (issues 65-67)

--- src/main/java/model/GameMap.java
+++ src/main/java/model/GameMap.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
+import common.GridPos;
 
-/**
- *
- * @author lenovo
- */
 public class GameMap {
-    
+    private final int width;
+    private final int height;
+
+    // #65 Implement GameMap grid structure
+    private final Tile[][] tiles;
+
+    public GameMap(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.tiles = new Tile[width][height];
+        initializeTiles();
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    private void initializeTiles() {
+        for (int x = 0; x < width; ++x) {
+            for (int y = 0; y < height; ++y) {
+                tiles[x][y] = null;
+            }
+        }
+    }
+
+    // #66 Implement map bounds checking
+    public boolean inBounds(GridPos pos) {
+        return pos.x >= 0 && pos.y >= 0 && pos.x < width && pos.y < height;
+    }
+
+    // #67 Implement tile lookup
+    public Tile getTile(GridPos pos) {
+        if (!inBounds(pos)) return null;
+        return tiles[pos.x][pos.y];
+    }
 }
