2ed3261849751ead280f4d92d65b768055099c1a
muna <munahssnn@gmail.com>
2026-03-17 23:55:30 +0100

    Completed issues with the label Core Simulation.

--- src/main/java/model/Company.java
+++ src/main/java/model/Company.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author lenovo
- */
 public class Company {
-    
+    public void tick(double deltaTime) {
+        if (Double.isNaN(deltaTime) || Double.isInfinite(deltaTime) || deltaTime <= 0.0) {
+            return;
+        }
+    }
 }
