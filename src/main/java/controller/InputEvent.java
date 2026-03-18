a0dd6a24f50926a2920f914b370591f138ed9766
Ashley <asuna.omizo@outlook.jp>
2026-03-18 23:34:37 +0100

    Implement InputEvent Class

--- src/main/java/controller/InputEvent.java
+++ src/main/java/controller/InputEvent.java
@@ -6,8 +6,32 @@ package controller;
 
 /**
  *
- * @author lenovo
+ * @author asuna
  */
+import common.GridPos;
+import common.Vec2;
+
 public class InputEvent {
-    
+
+    public String type;
+    public Vec2 mousePos;
+    public GridPos tilePos;
+    public String key;
+    public int mouseButton;
+
+    public GridPos dragStartTile;
+    public GridPos dragEndTile;
+
+    // Constructor for mouse event
+    public InputEvent(String type, Vec2 mousePos, int mouseButton) {
+        this.type = type;
+        this.mousePos = mousePos;
+        this.mouseButton = mouseButton;
+    }
+
+    // Constructor for keyboard event
+    public InputEvent(String type, String key) {
+        this.type = type;
+        this.key = key;
+    }
 }
