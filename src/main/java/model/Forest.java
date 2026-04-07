befd23ca59a274a6a52e924a039b0005b4a604b7
Qian Gu <guqian202309@gmail.com>
2026-04-07 20:05:38 +0200

    Implement Forests

--- src/main/java/model/Forest.java
+++ src/main/java/model/Forest.java
@@ -5,12 +5,40 @@
 package model;
 
 public class Forest extends Terrain {
-    @Override
-    public boolean isPassable() { return true; }
+
+    private int treeCount; // 1–4
+
+    public Forest() {
+        // initial tree amount 1–4
+        this.treeCount = 1 + (int)(Math.random() * 4);
+    }
+
+    public int getTreeCount() {
+        return treeCount;
+    }
+
+    public void grow() {
+        if (treeCount < 4) {
+            treeCount++;
+        }
+    }
+
+    public boolean canSpread() {
+        return treeCount == 4; // only when trees are full
+    }
 
     @Override
-    public double buildMultiplier() { return 1.5; }
+    public boolean isPassable() {
+        return true;
+    }
 
     @Override
-    public boolean isForest() {return true;}
+    public double buildMultiplier() {
+        return 1.5;
+    }
+
+    @Override
+    public boolean isForest() {
+        return true;
+    }
 }
