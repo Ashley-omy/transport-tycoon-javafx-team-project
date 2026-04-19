91b0777890736a8881dc7d9c4302ecb9cc959232
Ashley <asuna.omizo@outlook.jp>
2026-04-19 20:00:53 +0200

    Applied land graphic

--- src/main/java/view/TileRenderer.java
+++ src/main/java/view/TileRenderer.java
@@ -11,15 +11,21 @@ package view;
  */
 import common.Vec2;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import model.City;
 import model.Factory;
 import model.Mine;
 import model.Tile;
 
+import java.io.InputStream;
+
 public class TileRenderer {
+    private static final String LAND_TEXTURE_PATH = "/textures/land-tile.png";
+    private static final Image LAND_TILE_TEXTURE = loadLandTileTexture();
 
     public void drawTile(GraphicsContext gc, Tile t, Vec2 pos, int size) {
+        boolean baseTileAlreadyDrawn = false;
 
         // Simple color by terrain
         if (t.isWater()) {
@@ -27,10 +33,18 @@ public class TileRenderer {
         } else if (t.isForest()) {
             gc.setFill(Color.FORESTGREEN);
         } else {
-            gc.setFill(Color.GREEN);
+            if (LAND_TILE_TEXTURE != null && !LAND_TILE_TEXTURE.isError()) {
+                // Draw one land texture per tile.
+                gc.drawImage(LAND_TILE_TEXTURE, pos.x, pos.y, size, size);
+                baseTileAlreadyDrawn = true;
+            } else {
+                gc.setFill(Color.GREEN);
+            }
         }
 
-        gc.fillRect(pos.x, pos.y, size, size);
+        if (!baseTileAlreadyDrawn) {
+            gc.fillRect(pos.x, pos.y, size, size);
+        }
 
         // Road overlay
         if (t.getRoadPiece() != null) {
@@ -61,5 +75,16 @@ public class TileRenderer {
         // Grid border
         gc.setStroke(Color.DARKGREEN);
         gc.strokeRect(pos.x, pos.y, size, size);
+    }
+
+    private static Image loadLandTileTexture() {
+        try (InputStream stream = TileRenderer.class.getResourceAsStream(LAND_TEXTURE_PATH)) {
+            if (stream == null) {
+                return null;
+            }
+            return new Image(stream);
+        } catch (Exception ignored) {
+            return null;
+        }
     }
 }
