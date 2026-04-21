628784319ee51775e2ccabc3528415792226c55d
Ashley <asuna.omizo@outlook.jp>
2026-04-21 19:37:12 +0200

    Created non-trivial test case: The Factory Production Cycle

--- src/test/java/model/FactoryTest.java
+++ src/test/java/model/FactoryTest.java
@@ -1,12 +1,42 @@
 package model;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import common.Id;
+import common.Money;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FactoryTest {
 
     @Test
-    @Disabled("TODO")
-    void placeholder() {
+    void productionCycleConsumesInputProducesOutputAndStopsAtFullCapacity() {
+        Factory steelMill = Factory.createSteelMill(Id.genNew());
+        Id stopId = Id.genNew();
+
+        steelMill.acceptDelivery(new Shipment(
+                ShipmentKind.GOODS,
+                GoodsType.IRON,
+                30,
+                stopId,
+                stopId,
+                Money.of(1)
+        ));
+
+        int initialInput = steelMill.getInputStock();
+        int productionRate = steelMill.getProductionRate();
+
+        steelMill.tick(steelMill.getProductionTime());
+
+        assertEquals(initialInput - productionRate, steelMill.getInputStock());
+        assertEquals(productionRate, steelMill.getOutputStock());
+
+        steelMill.outputStock = steelMill.getMaxStockCapacity();
+        int inputBeforeBlockedTick = steelMill.getInputStock();
+
+        steelMill.tick(steelMill.getProductionTime() * 2.0);
+
+        assertEquals(steelMill.getMaxStockCapacity(), steelMill.getOutputStock());
+        assertEquals(inputBeforeBlockedTick, steelMill.getInputStock());
     }
 }
